6735d3bb791afea4b731467a6ebff51dacb89542
Kangmoon Seo <100016044+KangmoonSeo@users.noreply.github.com>
2024-03-07 01:28:57 +0900

    feat: 전수조사 추가

--- main.py
+++ main.py
@@ -1,16 +1,38 @@
 import random
 
 
-시행_횟수 = 1000
-살아남은_2등_인원 = 3
-로그_활성화 = True
+시행_횟수 = 10000
+살아남은_2등_인원 = 10
+로그_활성화 = False
 추가_점수공개_인원 = [
     # "부산 B",
+    # "부산 C",
     # "제주",
 ]
 
 
-def run_game() -> None:
+def run_game(살아남은_2등_인원: int) -> None:
+
+    유효_감지 = 0
+    text_log = ""
+
+    for _ in range(시행_횟수):
+        result, lg = play_once(살아남은_2등_인원=살아남은_2등_인원)
+
+        if result is True:
+            text_log = lg
+            유효_감지 += 1
+
+    if 로그_활성화 and len(text_log) > 1:
+        print(text_log)
+
+    print(
+        f"살아남은 2등 인원: 13명 중 {살아남은_2등_인원}명, 유효 감지: {유효_감지} ({round(유효_감지/시행_횟수*100)}%)"
+    )
+    return
+
+
+def play_once(살아남은_2등_인원: int):
 
     ballots = [
         "경기 A",
@@ -29,29 +51,6 @@ def run_game() -> None:
         "경남",
     ]
     ballots.extend(추가_점수공개_인원)
-
-    유효_감지 = 0
-    for _ in range(시행_횟수):
-        result = play_once(
-            로그_활성화=로그_활성화,
-            살아남은_2등_인원=살아남은_2등_인원,
-            ballots=ballots,
-        )
-
-        if result is True:
-            유효_감지 += 1
-
-    print(f"총 인원: {len(ballots)+1}명 ")
-    print("1차 합격 인원: 13명")
-    print(f"살아남은 2등 인원: 13명 중 {살아남은_2등_인원}명")
-    print(f"시행 횟수: {시행_횟수}")
-    print(f"유효 감지: {유효_감지}")
-
-    return
-
-
-def play_once(로그_활성화: bool, 살아남은_2등_인원: int, ballots: list):
-
     sampled_ballots = ["인천"]
     b = random.sample(ballots, len(ballots))
 
@@ -60,38 +59,38 @@ def play_once(로그_활성화: bool, 살아남은_2등_인원: int, ballots: li
     m = {}
     cnt = 0
     i = 0
-    text = ""
+    text = "======\n"
     for ballot in sampled_ballots:
-
-        ballot = ballot[:2]
-        if ballot in m:
-            m[ballot] += 1
-        else:
-            m[ballot] = 1
 
         i += 1
         text += f"{i}위: {ballot}"
 
-        if m[ballot] > 1:
+        ballot = ballot[:2]
+        if ballot in m:
+            m[ballot] += 1
             text += f" {m[ballot]}등"
+        else:
+            m[ballot] = 1
+
         text += "\t"
-        if i % 3 == 0:
+        if i % 5 == 0:
             text += "\n"
 
         if i <= 13 and m[ballot] > 1:
             cnt += 1
 
-    text += "\n======\n"
-
     if cnt == 살아남은_2등_인원:
+        return True, text
 
-        if 로그_활성화:
-            print(text)
-
-        return True
-
-    return False
+    return False, text
 
 
 if __name__ == "__main__":
-    run_game()
+
+    print(f"총 인원: {len(추가_점수공개_인원)+15}명 ")
+    print("1차 합격 인원: 13명")
+    print(f"시행 횟수: {시행_횟수}")
+
+    for 인원 in range(살아남은_2등_인원 + 1):
+
+        run_game(살아남은_2등_인원=인원)
